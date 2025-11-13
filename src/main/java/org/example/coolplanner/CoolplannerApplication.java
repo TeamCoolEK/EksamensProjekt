5c78b6a7b65c5a756dd29bf136f8fb75bb2fc2a6
Anoepus <ansu0001@stud.kea.dk>
2025-11-13 10:27:58 +0100

    coolplanners fødselsdag

---
+++ src/main/java/org/example/coolplanner/CoolplannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.coolplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/coolplanner/CoolplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coolplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
