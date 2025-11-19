8cd623ed352ace247750228aa3d52df55a8ba386
MaikenZachariasen <ma.zachariasen@gmail.com>
2025-11-19 13:22:48 +0100

    Tilføj UserStory GET og POST i CoolPlannerController

--- src/main/java/org/example/coolplanner/controller/CoolPlannerController.java
+++ src/main/java/org/example/coolplanner/controller/CoolPlannerController.java
@@ -1,6 +1,7 @@
 package org.example.coolplanner.controller;
 
 import org.example.coolplanner.model.Project;
+import org.example.coolplanner.model.UserStory;
 import org.example.coolplanner.service.CoolPlannerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,14 +21,26 @@ public class CoolPlannerController {
     }
 
     @GetMapping("/createProject")
-    public String createProject (Model model) {
+    public String createProject(Model model) {
         model.addAttribute("project", new Project());
         return "createProject";
     }
 
     @PostMapping("/saveProject")
-    public String saveProject (@ModelAttribute Project project, Model model) {
+    public String saveProject(@ModelAttribute Project project, Model model) {
         coolPlannerService.createProject(project);
+        return "redirect:/XYZ";
+    }
+
+    @GetMapping("/createUserStory")
+    public String createUserStory(Model model) {
+        model.addAttribute("userStory", new UserStory());
+        return "createUserrStory";
+    }
+
+    @PostMapping("/saveUserStory")
+    public String saveUserStory(@ModelAttribute UserStory userStory, Model model) {
+        coolPlannerService.createUserStory(userStory);
         return "redirect:/XYZ";
     }
 }
