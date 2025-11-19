d213d77e87aebf56798ae393d5c50681b4bec2b1
Anoepus <ansu0001@stud.kea.dk>
2025-11-19 10:50:22 +0100

    projekt model

--- src/main/java/org/example/coolplanner/model/Project.java
+++ src/main/java/org/example/coolplanner/model/Project.java
@@ -1,4 +1,25 @@
 package org.example.coolplanner.model;
 
+import java.util.Date;
+
 public class Project {
+    private int projectId;
+    private String projectName;
+    private String projectDetails;
+    private Date projectStartDate;
+    private Date projectDeadLine;
+    private int projectTimeEstimate;
+    private int projectActualTime;
+
+    public Project (int projectId, String projectName, String projectDetails, Date projectStartDate, Date projectDeadLine, int projectTimeEstimate, int projectActualTime) {
+        this.projectId = projectId;
+        this.projectName = projectName;
+        this.projectDetails = projectDetails;
+        this.projectStartDate = projectStartDate;
+        this.projectDeadLine = projectDeadLine;
+        this.projectTimeEstimate = projectTimeEstimate;
+        this.projectActualTime = projectActualTime;
+    }
+
+    public Project () {} //tom konstruktør
 }
