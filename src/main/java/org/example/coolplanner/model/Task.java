ff25f95b2629d6566e2fb302096e3d725826e79b
MaikenZachariasen <ma.zachariasen@gmail.com>
2025-11-24 11:19:39 +0100

    fra userstory til task

---
+++ src/main/java/org/example/coolplanner/model/Task.java
@@ -0,0 +1,91 @@
+package org.example.coolplanner.model;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private int taskID;
+    private String taskName;
+    private String taskDetails;
+    private LocalDate taskStartDate;
+    private LocalDate taskDeadline;
+    private int taskTimeEstimate;
+    private int taskActualTime;
+    private String taskStatus;
+    private int subprojectID;
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDetails() {
+        return taskDetails;
+    }
+
+    public void setTaskDetails(String taskDetails) {
+        this.taskDetails = taskDetails;
+    }
+
+    public LocalDate getTaskStartDate() {
+        return taskStartDate;
+    }
+
+    public void setTaskStartDate(LocalDate taskStartDate) {
+        this.taskStartDate = taskStartDate;
+    }
+
+    public LocalDate getTaskDeadline() {
+        return taskDeadline;
+    }
+
+    public void setTaskDeadline(LocalDate taskDeadline) {
+        this.taskDeadline = taskDeadline;
+    }
+
+    public int getTaskTimeEstimate() {
+        return taskTimeEstimate;
+    }
+
+    public void setTaskTimeEstimate(int taskTimeEstimate) { this.taskTimeEstimate = taskTimeEstimate; }
+
+    public int getTaskActualTime() {
+        return taskActualTime;
+    }
+
+    public void setTaskActualTime(int taskActualTime) {
+        this.taskActualTime = taskActualTime;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public int getSubprojectID() {
+        return subprojectID;
+    }
+
+    public void setSubprojectID(int subprojectID) {
+        this.subprojectID = subprojectID;
+    }
+
+    public Object getTaskById() {
+        return
+    }
+}
+
