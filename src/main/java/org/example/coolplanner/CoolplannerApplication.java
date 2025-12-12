c45c6ece306be0dc7ea204e5a16477e417921968
Anoepus <ansu0001@stud.kea.dk>
2025-12-12 12:44:51 +0100

    log

--- src/main/java/org/example/coolplanner/CoolplannerApplication.java
+++ src/main/java/org/example/coolplanner/CoolplannerApplication.java
@@ -1,13 +1,24 @@
 package org.example.coolplanner;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CoolplannerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoolplannerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner logDbUrl(DataSource dataSource) {
+        return args -> {
+            System.out.println("DB URL: " + dataSource.getConnection().getMetaData().getURL());
+        };
     }
 
 }
